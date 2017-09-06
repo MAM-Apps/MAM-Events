259ee7d3284e76735c141cc546a7f32da1df68cd
Max Gerber <mgerber94@gmail.com>
2017-09-06 15:26:19 +0100

    Set up basic server with handlebars working This allows us to progress into creating a basic homepage, and playing with Twitter's Streaming API Relates #19

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const router = express.Router();
+
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
