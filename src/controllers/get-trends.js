91683463301f39c63c18a7dcd2ab0f74567824ad
AseelM <aseelmustafa@outlook.com>
2017-09-07 12:10:59 +0100

    built logic to obtain twitter trends from api #3

---
+++ src/controllers/get-trends.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const get = () => {
+  const options = {
+    url: "https://api.twitter.com/1.1/trends/place.json?id=1",
+    headers: {
+      Authorization: 'OAuth oauth_consumer_key="yURhm5VsOTRNrX1IHIyvp6f75",oauth_token="70442620-bxmJnJBuqxuZtm4jacgCTN07MGrNlgzRjh9vlt0fx",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1504782519",oauth_nonce="nsIMKV",oauth_version="1.0",oauth_signature="LJU6WxnZtH1HWnM8ulFjrOngOlk%3D"'
+
+    }
+  }
+  request.get(options, (err, response, body) => {
+    if (err) {
+      console.log(err);
+    }else {
+      console.log(body);
+    }
+  })
+
+}
+get();
