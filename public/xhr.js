fc08b4f4b49b976707b0b72b4e7b44dd276677e5
Max Gerber <mgerber94@gmail.com>
2017-09-12 00:42:41 +0100

    Added functionality to allow to search by time of event Relates #36

--- public/xhr.js
+++ public/xhr.js
@@ -3,10 +3,14 @@ var form = document.getElementById('map-form');
 var lat = document.getElementById('lat');
 var lng = document.getElementById('lng');
 var address = document.getElementById('address');
+var until = document.getElementById('until');
 
 var distance = document.getElementById('distance');
 var xhrRequest = function(cb) {
-  var url = '/events/?address=' + address.value;
+var dateTime = new Date(until.value).getTime();
+var timestamp = Math.floor(dateTime / 1000);
+  var url = '/events/?address=' + address.value + "&until=" + timestamp;
+  console.log(url);
   // var url = '/events/?' + 'lat=' + lat.value + '&lng=' + lng.value + '&distance=' + distance.value;
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
